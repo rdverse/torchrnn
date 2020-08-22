a6ae9eba62fdf05a5bb7732a79eef7b174821bb7
ReDev <deveshreddy27@gmail.com>
2020-08-22 18:06:03 -0500

    adding files

---
+++ Data.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+class Data():
+    def __init__(self):
+        self.dataSize = 10000
+        self.features = self._generate_data('features')
+        self.labels = self._generate_data('labels')
+
+    def _generate_data(self, type):
+        if type == 'features':
+            data = np.random.randint(-10, 10, (self.dataSize, 512))
+            return (data.astype(np.float32))
+        elif type == 'labels':
+            data = np.random.randint(2, size=self.dataSize)
+            return (data.astype(np.int))
+
+    def getData(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.features,
+                                                            self.labels,
+                                                            test_size=0.2,
+                                                            random_state=42)
+        return (X_train, X_test, y_train, y_test)
