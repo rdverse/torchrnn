9b25c11eb6a6b180d381c7db2919876dea1828d2
ReDev <deveshreddy27@gmail.com>
2020-08-23 21:47:52 -0500

    add LSTM

--- model.py
+++ model.py
@@ -24,10 +24,20 @@ class Net(nn.Module):
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        out, _ = self.rnn1(x, h0)
+
+        # uncoment for for lstm
+        #c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+
+        # for gru and rnn
+        #out, _ = self.rnn1(x, h0)
+
+        # for lstm
+        #out, _ = self.rnn1(x, (h0, c0))
+
         out = torch.reshape(out, (out.shape[0], -1))
         out = F.relu(self.fc1(out))
         out = F.relu(self.fc2(out))
+
         # BCE with logits combines a sigmoid layer for the output node
         out = self.fc3(out)
         return out
